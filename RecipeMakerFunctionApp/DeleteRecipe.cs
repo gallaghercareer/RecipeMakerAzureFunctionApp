7939ba9be93fce4e4da2cd73a1b61903aef81b9b
John Gallagher <gallaghercareer@gmail.com>
2026-01-03 16:09:11 -0500

    added deleterecipe

--- RecipeMakerFunctionApp/DeleteRecipe.cs
+++ RecipeMakerFunctionApp/DeleteRecipe.cs
@@ -1,7 +1,9 @@
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace RecipeMakerFunctionApp;
 
@@ -15,9 +17,44 @@ public class DeleteRecipe
     }
 
     [Function("DeleteRecipe")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "delete")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        string userId = "local-chef-123";
+
+    #if !DEBUG
+                if (req.Headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-ID", out var principalIds))
+                {
+                    userId = principalIds.FirstOrDefault();
+                }
+    #endif
+
+        
+        //get recipe entity from the request
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var data = JsonSerializer.Deserialize<RecipeEntity>(requestBody, options);
+
+
+        if (data == null || string.IsNullOrEmpty(data.RowKey))
+        {
+            return new BadRequestObjectResult("Recipe ID (RowKey) is required.");
+        }
+
+        try
+        {
+            //delete from table using rowkey (my recipe guid)
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            TableClient tableClient = new TableClient(connectionString, "Recipes");
+            await tableClient.DeleteEntityAsync(userId, data.RowKey);
+
+            _logger.LogInformation($"Deleted recipe {data.RowKey} for user {userId}");
+
+            return new NoContentResult(); // Success (204)
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Delete failed: {ex.Message}");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError); //500 Failure
+        }
     }
 }
